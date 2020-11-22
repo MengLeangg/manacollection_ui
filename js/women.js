0bb0b8fc33a1a35c21215f5eeb6df4124881904a
Mengleangg <mengleangtoung9@gmail.com>
2020-11-22 13:35:16 +0700

    create filter and sortby for mobile button dropdown

--- js/women.js
+++ js/women.js
@@ -5,4 +5,22 @@ $(document).ready(function() {
             $('.category-desc-mini').toggleClass('collapsed');
         }
     });
+
+    // -- Desktop Sort Filter Drop -- //
+    $('.sort-filter-wrapper .filter-label').on('click', function(e) {
+        e.stopPropagation();
+        $('.sort-filter-wrapper').toggleClass('active');
+        $(this).children('i').toggleClass('active');
+        $('.sort-filter-wrapper .filter-container').toggle();
+        $('.filters .filter-label.active').removeClass('active');
+        $('.filters .filter-label.active i.fa-chevron-down').addClass('active');
+        $('.filters .filter-label.active i.fa-chevron-up').removeClass('active');
+        $('.filters .filter-container.active').removeClass('active');
+        $(document).on('click', function(e) {
+            $('.sort-filter-wrapper').removeClass('active');
+            $('.sort-filter-wrapper .filter-container').hide();
+            $('.sort-filter-wrapper .filter-label i.fa-chevron-down').addClass('active');
+            $('.sort-filter-wrapper .filter-label i.fa-chevron-up').removeClass('active');
+        });
+    });
 })
