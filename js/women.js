5d04d13ce6249011175388a981d74b7ae4d8e5f5
Mengleangg <mengleangtuong.it@gmail.com>
2020-11-22 17:57:09 +0700

    sssss

--- js/women.js
+++ js/women.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+    // Global variable
+    let filterParentDiv = $('.product-browse .products .products-display .display-nav-wrapper');
+
     // -- Category Description Show More -- //
     $('.category-desc-mini').on('click', function() {
         if (!$(this).hasClass('disabled')) {
@@ -22,5 +26,29 @@ $(document).ready(function() {
             $('.sort-filter-wrapper .filter-label i.fa-chevron-down').addClass('active');
             $('.sort-filter-wrapper .filter-label i.fa-chevron-up').removeClass('active');
         });
+    });
+
+    // -- Mobile Filters Opened -- //
+    $('.products-display .mobile-buttons .mb-filters').on('click', function() {
+        if ($('.products-display .mobile-buttons').hasClass('follow')) {
+            $('html, body').animate({ scrollTop: '94' }, function() {
+                filterParentDiv.slideDown('fast');
+                filterParentDiv.addClass('active');
+                $(this).children('img').addClass('flipped');
+                $(this).addClass('dropped');
+            });
+        }
+        else {
+            filterParentDiv.slideToggle('fast');
+            filterParentDiv.toggleClass('active');
+            $('.show-new-products').removeClass('active');
+            $(this).children('img').toggleClass('flipped');
+            $(this).toggleClass('dropped');
+        }
+    });
+    $('.products-display .display-nav-wrapper .close.mobile').on('click', function() {
+        filterParentDiv.slideUp();
+        $('.show-new-products').removeClass('active');
+        filterParentDiv.removeClass('active');
     });
 })
